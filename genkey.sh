ef296ab1d4a67ca5417d2f9abdf32283dc8f0193
hipye <i@omoe.cc>
2022-02-24 23:41:08 +0800

    初始化

---
+++ genkey.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+#sed -i 's/\[STYLE239\]\nSHOW=、/\[STYLE239\]\nSHOW=?/g' default.css
+#sed -i 's/\[STYLE172\]\nSHOW=（/\[STYLE172\]\nSHOW=(/g' default.css
+#sed -i 's/\[STYLE173\]\nSHOW=）/\[STYLE173\]\nSHOW=)/g' default.css
+##中文键
+#sed -i 's/\[STYLExx\]\nSHOW=Q/\[STYLExx\]\nSHOW=q/g' default.css
+
